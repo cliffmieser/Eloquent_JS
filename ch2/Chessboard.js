822078133e98d338f9bb16b693e0636c8a3403c9
cliffmieser <cliffmieser@gmail.com>
2022-05-01 11:38:45 -0400

    Add Chessboard.js

---
+++ ch2/Chessboard.js
@@ -0,0 +1,7 @@
+/*Write a program that creates a string that represents an 8×8 grid, 
+using newline characters to separate lines. At each position of the 
+grid there is either a space or a "#" character.
+The characters should form a chessboard.*/
+
+let size = 8, str = "";
+
